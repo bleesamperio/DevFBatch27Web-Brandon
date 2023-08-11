e051cfc72bd1cb37bbc3a4642e4811d3bfa7ecdb
bleesamperio <bleesamperio@outlook.com>
2023-08-10 21:39:43 -0600

    Primeros ejercicios

---
+++ Funciones/funcion.js
@@ -0,0 +1,15 @@
+function saludar() {
+    let mensaje = "¡Hola!";
+    return mensaje;
+}
+
+//Mandar llamar o ejecutar
+console.log(saludar());
+
+//LAS FUNCIONES PIDEN PARAMETROS
+function suma(a, b) {
+    
+}
+
+// Y RECIBEN ARGUMENTOS
+    suma(7, 9)
